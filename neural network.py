c78ec2a0875eeebb6fd6d784945b37f6c1a0a2be
Sebastian Felipe Alvarez Montoya <69206936+SebastianAlvarez96@users.noreply.github.com>
2021-11-18 07:42:50 -0500

    Update neural network.py

--- neural network.py
+++ neural network.py
@@ -1,3 +1,14 @@
-git clone https://github.com/mnielsen/neural-networks-and-deep-learning.git
+git clone https://github.com/mnielsen/neural-networks-and-deep-learning.git 
   
+import numpy as np
+
+class Network(object):
+
+    def __init__(self, sizes):
+        self.num_layers = len(sizes)
+        self.sizes = sizes
+        self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
+        self.weights = [np.random.randn(y, x) 
+                        for x, y in zip(sizes[:-1], sizes[1:])]
   
+net = Network([2, 3, 1])
